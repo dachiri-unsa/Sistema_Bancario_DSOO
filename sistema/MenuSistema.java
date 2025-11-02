c45ff9455b6922aa4442a40fb090d07281bc9d84
Xdaniel <dachiri@unsa.edu.pe>
2025-11-02 18:18:30 -0500

    menu principal opciones

--- sistema/MenuSistema.java
+++ sistema/MenuSistema.java
@@ -1,6 +1,5 @@
 package sistema;
 
-import sistema.Banco;
 import java.util.Scanner;
 
 public class MenuSistema {
@@ -22,6 +21,10 @@ public class MenuSistema {
             System.out.println("Ingrese una opcion: ");
             System.out.println("1. Gestion de clientes.");
             System.out.println("2. Gestion de cuentas.");
+            System.out.println("3. Gestion de tarjetas.");
+            System.out.println("4. Cajero automatico.");
+            System.out.println("5. Gestion de Empleados.");
+            System.out.println("6. Gestion de cajeros.");
             System.out.println("0. Salir.");
             opcion = sc.nextLine();
             switch (opcion) {
@@ -31,6 +34,7 @@ public class MenuSistema {
                 case "2":
                     menuCuentas.mostrarMenuCuentas();
                     break;
+                //Aqui demas casos.
                 default:
                     System.out.println("Eleccion no valida. Por favor volver a ingresar.");
                     break;
