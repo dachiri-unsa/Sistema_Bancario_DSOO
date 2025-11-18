f8aedf2812a4dcbda60cc0ef08c39f223e8dbd85
Xdaniel <dachiri@unsa.edu.pe>
2025-11-18 13:21:18 -0500

    Validacion de las cuentas

--- sistema/MenuCuentas.java
+++ sistema/MenuCuentas.java
@@ -1,10 +1,12 @@
 package sistema;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import entidades.Cliente;
 import entidades.CuentaBancaria;
 import entidades.TipoMoneda;
+import gestores.GestorCuentasBancarias;
 // CuentaBancaria
 public class MenuCuentas {
     private Scanner sc;
@@ -81,23 +83,35 @@ public class MenuCuentas {
         if (cliente == null) {
             return;
         }
-        if (cliente.getGestorCuentasBancarias().getCuentas().isEmpty()) {
+        GestorCuentasBancarias gestor = cliente.getGestorCuentasBancarias();
+        if (gestor.getCuentas().isEmpty()) {
             System.out.println("El cliente no tiene cuentas disponibles.");
             return;
         }
-        System.out.println("Las cuentas del cliente: ");
-        cliente.getGestorCuentasBancarias().listarCuentas();
-        System.out.println("Ingresar el numero de la cuenta a consultar.");
-        String numeroCuenta = sc.nextLine().trim();
-        if (numeroCuenta.isEmpty()) {
-            System.out.println("El numero de cuenta no puede estar vacio.");
-            return;
-        }
-        for(CuentaBancaria c : cliente.getGestorCuentasBancarias().getCuentas()) {
-            if (c.getNumeroCuenta().equals(numeroCuenta)) {
-                System.out.println("El saldo disponible en esa cuenta es: "+c.getSaldo());
+        String numeroCuenta;
+        if(gestor.getCuentas().size() > 1) {
+            System.out.println("Las cuentas del cliente: ");
+            gestor.listarCuentas();
+            System.out.println("Ingresar el numero de la cuenta a consultar.");
+            numeroCuenta = sc.nextLine().trim();
+            if (numeroCuenta.isEmpty()) {
+                System.out.println("El numero de cuenta no puede estar vacio.");
                 return;
             }
+            if (!numeroCuenta.matches("\\d+")) {
+                System.out.println("Solo debe contener números.");
+            }
+            for(CuentaBancaria c : gestor.getCuentas()) {
+                if (c.getNumeroCuenta().equals(numeroCuenta)) {
+                    System.out.println("El saldo disponible en esa cuenta es: "+c.getSaldo());
+                    return;
+                }
+            }
+        }
+        else {
+            CuentaBancaria c = gestor.getCuentas().get(0);
+            System.out.println("El saldo disponible de la cuenta "+c.getNumeroCuenta()+" es: "+c.getSaldo());
+            return;
         }
         System.out.println("Numero de cuenta no encontrado.");
     }
@@ -107,19 +121,35 @@ public class MenuCuentas {
         if (cliente == null) {
             return;
         }
-        System.out.println("Las cuentas del cliente: ");
-        cliente.getGestorCuentasBancarias().listarCuentas();
-        System.out.println("Ingresar el numero de la cuenta a consultar.");
-        String numeroCuenta = sc.nextLine().trim();
-        if (numeroCuenta.isEmpty()) {
-            System.out.println("El numero de cuenta no puede estar vacio.");
+        GestorCuentasBancarias gestor = cliente.getGestorCuentasBancarias();
+        if (gestor.getCuentas().isEmpty()) {
+            System.out.println("El cliente no tiene cuentas disponibles.");
             return;
         }
-        for(CuentaBancaria c : cliente.getGestorCuentasBancarias().getCuentas()) {
-            if (c.getNumeroCuenta().equals(numeroCuenta)) {
-                c.getHistorial().listarMovimientos();
+        String numeroCuenta;
+        if(gestor.getCuentas().size() > 1) {
+            System.out.println("Las cuentas del cliente: ");
+            gestor.listarCuentas();
+            System.out.println("Ingresar el numero de la cuenta a consultar.");
+            numeroCuenta = sc.nextLine().trim();
+            if (numeroCuenta.isEmpty()) {
+                System.out.println("El numero de cuenta no puede estar vacio.");
                 return;
             }
+            if (!numeroCuenta.matches("\\d+")) {
+                System.out.println("Solo debe contener números.");
+            }
+            for(CuentaBancaria c : gestor.getCuentas()) {
+                if (c.getNumeroCuenta().equals(numeroCuenta)) {
+                    c.getHistorial().listarMovimientos();
+                    return;
+                }
+            }
+        }
+        else {
+            CuentaBancaria c = gestor.getCuentas().get(0);
+            System.out.println("El saldo disponible de la cuenta "+c.getNumeroCuenta()+" es: "+c.getSaldo());
+            return;
         }
         System.out.println("Numero de cuenta no encontrado.");
     }
@@ -127,7 +157,11 @@ public class MenuCuentas {
         System.out.println("Ingrese el DNI del cliente: ");
         String dni = sc.nextLine().trim();
         if (dni.isEmpty()) {
-            System.out.println("El dni no puede estar vacio.");
+            System.out.println("El DNI no puede estar vacio.");
+            return null;
+        }
+        if (!dni.matches("\\d{8}")) {
+            System.out.println("El DNI debe contener exactamente 8 dígitos numéricos.");
             return null;
         }
         Cliente cliente = banco.getGestorClientes().buscarCliente(dni);
